1ba34eddc4406d1b57756c97cb43c964d0d80caf
Vish Patel <vpatel@and.digital>
2020-04-02 13:39:15 +0100

    init react native

--- src/styles/Theme.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const themeColors = {
-  primary: '#00A588',
-  primaryDark: '#007269',
-  background: '#F3EDE7',
-  text: '#666666'
-};
--- src/utils/Fonts.js
+++
@@ -1,5 +0,0 @@
-export const Fonts = {
-  Rubik: 'Rubik',
-  RubikLight: 'Rubik-Light',
-  RubikBold: 'Rubik-Medium',
-}

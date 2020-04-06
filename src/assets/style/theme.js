59f02f109106822ada88dfa77fd1a3970b135b5d
Vish Patel <vpatel@and.digital>
2020-04-06 16:09:19 +0100

    added colors to theme

--- src/assets/style/theme.js
+++ src/assets/style/theme.js
@@ -4,8 +4,21 @@ export default {
     body: 'Khula-Regular',
   },
   color: {
-    main: '#332ad8',
+    purpleDark: '#070544',
+    purple: '#361D9D',
+    pink: '#E04483',
+    pinkLight: '#E67FC4',
+    blue: '#4361EE',
+    blueLight: '#4ba9f9',
+    yellow: '#FAC140',
+    yellowLight: '#FFF376',
+    beige: '#FDF9ED',
+
+    main: '#332AD8',
     dark: '#323232',
-    light: '#ebeaf7',
+    light: '#FDF9ED',
+
+    white: '#FFF',
+    black: '#000',
   },
 };

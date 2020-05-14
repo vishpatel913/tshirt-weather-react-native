f2c633a6f9412d59f6ffda13e6310d0667c301ba
Vish Patel <vishpatel913@googlemail.com>
2020-05-14 14:34:31 +0100

    cleaned up data page structure and added more v padding

--- src/scenes/Data.tsx
+++ src/scenes/Data.tsx
@@ -17,85 +17,81 @@ interface Props {
 
 const PageLayout = styled(Layout)`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
 `;
-const SectionView = styled.View``;
 
 const Data = ({ weather }: Props) => {
   const { current, daily, hourly } = weather;
+  const detailsData = [
+    {
+      text: 'Feels Like',
+      icon: 'thermometer',
+      value: current ? Math.ceil(current.feels_like) : undefined,
+      unit: '°',
+    },
+    {
+      text: 'Cloud Cover',
+      icon: 'cloud',
+      value: current?.clouds,
+      unit: '%',
+    },
+    {
+      text: 'UV Index',
+      icon: 'hot',
+      value: current?.uvi,
+    },
+    {
+      text: 'Wind Speed',
+      icon: 'wind',
+      value: current?.wind_speed,
+      unit: 'm/s',
+    },
+    {
+      text: 'Humidity',
+      icon: 'humidity',
+      value: current?.humidity,
+      unit: '%',
+    },
+    {
+      text: 'Pressure',
+      icon: 'barometer',
+      value: current?.pressure,
+      unit: 'hPa',
+    },
+  ];
   const precip = daily?.[0].rain ? 'rain' : daily?.[0].snow ? 'snow' : null;
 
   return (
     <PageLayout>
-      <SectionView>
-        <Section title="Details">
-          <DetailTiles
-            data={[
-              {
-                text: 'Feels Like',
-                icon: 'thermometer',
-                value: current ? Math.ceil(current.feels_like) : undefined,
-                unit: '°',
-              },
-              {
-                text: 'Cloud Cover',
-                icon: 'cloud',
-                value: current?.clouds,
-                unit: '%',
-              },
-              {
-                text: 'UV Index',
-                icon: 'hot',
-                value: current?.uvi,
-              },
-              {
-                text: 'Wind Speed',
-                icon: 'wind',
-                value: current?.wind_speed,
-                unit: 'm/s',
-              },
-              {
-                text: 'Humidity',
-                icon: 'humidity',
-                value: current?.humidity,
-                unit: '%',
-              },
-              {
-                text: 'Pressure',
-                icon: 'barometer',
-                value: current?.pressure,
-                unit: 'hPa',
-              },
-            ]}
-          />
-        </Section>
-        {precip && (
-          <Section title={precip || 'rain'}>
-            <HourlyGraph
-              data={hourly?.map((item) => ({
-                x: moment.unix(item.dt).format('ha'),
-                y: (item.rain || item.snow) ?? 0,
-                icon: item.rain ? 300 : item.snow ? 600 : undefined,
-                additional:
-                  item.rain || item.snow
-                    ? `${item.rain || item.snow}mm`
-                    : undefined,
-              }))}
-            />
-          </Section>
-        )}
-        <Section title="Next 7 days">
-          <DailyForecast
-            data={daily?.map((item) => ({
-              timestamp: item.dt,
-              icon: item.weather[0].id,
-              tempMax: item.temp.max,
-              tempMin: item.temp.min,
-              additional: item.rain || item.snow || undefined,
+      <Section title="Details">
+        <DetailTiles data={detailsData} />
+      </Section>
+      {precip && (
+        <Section title={precip || 'rain'}>
+          <HourlyGraph
+            data={hourly?.map((item) => ({
+              x: moment.unix(item.dt).format('ha'),
+              y: (item.rain || item.snow) ?? 0,
+              icon: item.rain ? 300 : item.snow ? 600 : undefined,
+              additional:
+                item.rain || item.snow
+                  ? `${item.rain || item.snow}mm`
+                  : undefined,
             }))}
           />
         </Section>
-      </SectionView>
+      )}
+      <Section title="Next 7 days">
+        <DailyForecast
+          data={daily?.map((item) => ({
+            timestamp: item.dt,
+            icon: item.weather[0].id,
+            tempMax: item.temp.max,
+            tempMin: item.temp.min,
+            additional: item.rain || item.snow || undefined,
+          }))}
+        />
+      </Section>
     </PageLayout>
   );
 };

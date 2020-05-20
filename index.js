a3cd5d4c87845dcb6224938721428e1175e0bb2d
Vish Patel <vishpatel913@googlemail.com>
2020-05-21 00:42:59 +0100

    removed stroybook

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
-import Storybook from './storybook';
 
-const runStorybook = false;
-AppRegistry.registerComponent(appName, () => (runStorybook ? Storybook : App));
+AppRegistry.registerComponent(appName, () => App);
